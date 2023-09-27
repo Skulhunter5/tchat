58d7dd50009bcb87e698c1f5b0056a22089c37e6
Skulhunter5 <linus.huster@gmail.com>
2023-09-27 18:53:30 +0200

    Added Interface trait for modular setup

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,63 @@
 use std::fs::File;
-use std::io;
+use std::io::{self, Result};
 use std::io::{Read, Write};
 use crossterm::{event, ExecutableCommand, terminal};
 use crossterm::cursor::{Hide, Show};
 use crossterm::event::{Event, KeyCode, KeyEvent};
 use crossterm::terminal::{Clear, ClearType};
 
+enum InterfaceResult {
+    None,
+    Terminate,
+}
+
+trait Interface {
+    fn handle_keypress(&self, event: KeyEvent) -> Result<InterfaceResult>;
+}
+
+struct Window {
+    width: u16,
+    height: u16,
+}
+
+impl Window {
+    fn new(width: u16, height: u16) -> Self {
+        Self {
+            width,
+            height,
+        }
+    }
+}
+
+impl Interface for Window {
+    fn handle_keypress(&self, event: KeyEvent) -> Result<InterfaceResult> {
+        let mut stdout = io::stdout();
+
+        match event.code {
+            KeyCode::Esc | KeyCode::Char('q') => {
+                return Ok(InterfaceResult::Terminate);
+            }
+            KeyCode::Char(c) => {
+                stdout.execute(Clear(ClearType::CurrentLine)).unwrap();
+                print!("Pressed: {}\r", c);
+                stdout.flush()?;
+            }
+            code => {
+                stdout.execute(Clear(ClearType::CurrentLine)).unwrap();
+                print!("Pressed code: {:?}\r", code);
+                stdout.flush()?;
+            }
+        }
+        Ok(InterfaceResult::None)
+    }
+}
+
+fn jump_to(x: u16, y: u16) -> Result<()> {
+    print!("\x1b[{};{}H", y, x);
+    io::stdout().flush()
+}
+
 fn main() -> io::Result<()> {
-    let mut stdin = io::stdin();
     let mut stdout = io::stdout();
 
     terminal::enable_raw_mode().unwrap();
@@ -24,23 +74,18 @@ fn main() -> io::Result<()> {
     // specific row {n} and column {m}: \x1b[{n};{m}H
 
     let (width, height) = terminal::size()?;
+    let window = Window::new(width, height);
+    jump_to(window.width-1, window.height-1)?;
     loop {
         if event::poll(std::time::Duration::from_millis(100))? {
             match event::read()? {
                 Event::Key { 0: key_event } => {
-                    match key_event.code {
-                        KeyCode::Esc | KeyCode::Char('q') => {
+                    let result = window.handle_keypress(key_event);
+                    match result? {
+                        InterfaceResult::Terminate => {
                             break;
                         }
-                        KeyCode::Char(c) => {
-                            stdout.execute(Clear(ClearType::CurrentLine)).unwrap();
-                            print!("Pressed: {}\r", c);
-                            stdout.flush()?;
-                        }
-                        code => {
-                            print!("Pressed code: {:?}\r", code);
-                            stdout.flush()?;
-                        }
+                        InterfaceResult::None => {}
                     }
                 }
                 Event::Resize { 0: width, 1: height } => {
