458deb0764fe201619bc5cba4f86eb6600353cfe
Skulhunter5 <linus.huster@gmail.com>
2024-01-04 02:46:30 +0100

    Create initial prompt logic

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::fs::File;
+use crossterm::cursor::{Hide, MoveTo, Show};
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
+use crossterm::style::Print;
+use crossterm::terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::{event, terminal, QueueableCommand as _};
 use std::io::{self, Result};
-use std::io::{Read, Write};
-use crossterm::{event, ExecutableCommand, terminal};
-use crossterm::cursor::{Hide, Show};
-use crossterm::event::{Event, KeyCode, KeyEvent};
-use crossterm::terminal::{Clear, ClearType};
+use std::io::{stdout, Write};
+use std::thread;
+use std::time::Duration;
 
 enum InterfaceResult {
     None,
@@ -12,57 +14,76 @@ enum InterfaceResult {
 }
 
 trait Interface {
-    fn handle_keypress(&self, event: KeyEvent) -> Result<InterfaceResult>;
+    fn handle_keypress(&mut self, event: KeyEvent) -> Result<InterfaceResult>;
+    fn render(&self) -> Result<()>;
 }
 
 struct Window {
     width: u16,
     height: u16,
+    prompt: String,
+    history: Vec<String>,
 }
 
 impl Window {
     fn new(width: u16, height: u16) -> Self {
+        let prompt = String::new();
+        let history = Vec::new();
+
         Self {
             width,
             height,
+            prompt,
+            history,
         }
     }
 }
 
 impl Interface for Window {
-    fn handle_keypress(&self, event: KeyEvent) -> Result<InterfaceResult> {
-        let mut stdout = io::stdout();
-
+    fn handle_keypress(&mut self, event: KeyEvent) -> Result<InterfaceResult> {
         match event.code {
-            KeyCode::Esc | KeyCode::Char('q') => {
+            KeyCode::Esc => {
+                self.width;
                 return Ok(InterfaceResult::Terminate);
             }
+            KeyCode::Backspace => {
+                self.prompt.pop();
+            }
             KeyCode::Char(c) => {
-                stdout.execute(Clear(ClearType::CurrentLine)).unwrap();
-                print!("Pressed: {}\r", c);
-                stdout.flush()?;
+                if c == 'c' && event.modifiers.contains(KeyModifiers::CONTROL) {
+                    return Ok(InterfaceResult::Terminate);
+                } else {
+                    self.prompt.push(c);
+                }
+            }
+            KeyCode::Enter => {
+                let old_prompt = std::mem::replace(&mut self.prompt, String::new());
+                self.history.push(old_prompt);
             }
             code => {
-                stdout.execute(Clear(ClearType::CurrentLine)).unwrap();
-                print!("Pressed code: {:?}\r", code);
-                stdout.flush()?;
+                self.prompt = format!("Unhandled keycode: {:?}", code);
             }
         }
         Ok(InterfaceResult::None)
     }
-}
 
-fn jump_to(x: u16, y: u16) -> Result<()> {
-    print!("\x1b[{};{}H", y, x);
-    io::stdout().flush()
+    fn render(&self) -> Result<()> {
+        let mut stdout = stdout();
+        // TODO: display
+        stdout.queue(MoveTo(0, self.height))?;
+        stdout.queue(Clear(ClearType::CurrentLine))?;
+        stdout.queue(Print(&self.prompt))?;
+        Ok(())
+    }
 }
 
 fn main() -> io::Result<()> {
-    let mut stdout = io::stdout();
+    let mut stdout = stdout();
 
-    terminal::enable_raw_mode().unwrap();
-    stdout.execute(Hide).unwrap();
-    stdout.execute(Clear(ClearType::All)).unwrap();
+    terminal::enable_raw_mode()?;
+    stdout.queue(EnterAlternateScreen)?;
+    stdout.queue(Hide)?;
+    stdout.flush()?;
 
     // down by {n}: \x1b[{n}B
     // up by {n}: \x1b[{n}A
@@ -74,21 +95,24 @@ fn main() -> io::Result<()> {
     // specific row {n} and column {m}: \x1b[{n};{m}H
 
     let (width, height) = terminal::size()?;
-    let window = Window::new(width, height);
-    jump_to(window.width-1, window.height-1)?;
-    loop {
-        if event::poll(std::time::Duration::from_millis(100))? {
+    let mut window = Window::new(width, height);
+    stdout.queue(MoveTo(0, height))?;
+    'outer: loop {
+        while event::poll(std::time::Duration::ZERO)? {
             match event::read()? {
                 Event::Key { 0: key_event } => {
-                    let result = window.handle_keypress(key_event);
-                    match result? {
+                    let result = window.handle_keypress(key_event)?;
+                    match result {
                         InterfaceResult::Terminate => {
-                            break;
+                            break 'outer;
                         }
                         InterfaceResult::None => {}
                     }
                 }
-                Event::Resize { 0: width, 1: height } => {
+                Event::Resize {
+                    0: width,
+                    1: height,
+                } => {
                     println!("New Size: {}x{}\r", width, height);
                 }
                 e => {
@@ -96,14 +120,15 @@ fn main() -> io::Result<()> {
                 }
             }
         }
+        window.render()?;
+        stdout.flush()?;
+        thread::sleep(Duration::from_millis(16));
     }
 
-    //stdout.execute(Clear(ClearType::All))?;
-    print!("\x1b[2J");
+    stdout.queue(LeaveAlternateScreen)?;
+    stdout.queue(Show)?;
     stdout.flush()?;
     terminal::disable_raw_mode().unwrap();
-    stdout.execute(Show).unwrap();
-    println!();
 
     Ok(())
 }
